3f36acf0de15b39924755af19af837e3e8746684
topunixguy <topunixguy@gmail.com>
2020-05-01 12:45:59 -0400

    Create repeated_string.py

---
+++ Interview-Preparation-Kit/Warm-up-Challenges/repeated_string.py
@@ -0,0 +1,25 @@
+from collections import Counter
+
+# Complete the repeatedString function below.
+def repeatedString(s, n):
+    l = len(s)
+    counts = Counter(s)
+    nd = n//l
+    rm = n - (l * nd)
+    counta = Counter(s[:rm])
+    na = counta['a'] + (nd * counts['a']) 
+    return na
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    n = int(input())
+
+    result = repeatedString(s, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
