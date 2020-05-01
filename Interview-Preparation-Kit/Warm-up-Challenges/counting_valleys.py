f8bdfc51ebf9b3a658204d8d55a817ff2dbbe9f9
topunixguy <topunixguy@gmail.com>
2020-05-01 12:42:42 -0400

    Create counting_valleys.py

---
+++ Interview-Preparation-Kit/Warm-up-Challenges/counting_valleys.py
@@ -0,0 +1,46 @@
+# Complete the countingValleys function below.
+def countingValleys(n, s):
+    steps = list(s)
+    up = down = sea_level = valleys = 0
+    for step in steps:
+        if step == 'U':
+            up += 1
+        else:
+            down += 1
+        
+        if up == down:
+            if sea_level == -1:
+                if up == 1 and step == 'U':
+                    valleys += 1
+            # reset
+            sea_level = up = down = 0
+            continue
+        elif up > down:
+            sea_level = 1
+            continue
+        elif down > up:
+            # Below sea level
+            if sea_level >= 0:
+                sea_level = -1
+                up = 0
+                continue
+            elif sea_level == -1:
+                if up == 1 and step == 'U':
+                    valleys += 1
+                    continue
+    
+    return valleys
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    s = input()
+
+    result = countingValleys(n, s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
