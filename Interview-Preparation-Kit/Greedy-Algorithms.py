c5b90aafbfdf9089ab5795d66c0609476666f549
topunixguy <topunixguy@gmail.com>
2020-05-01 11:45:01 -0400

    Create Greedy-Algorithms.py

---
+++ Interview-Preparation-Kit/Greedy-Algorithms.py
@@ -0,0 +1,17 @@
+# Complete the minimumAbsoluteDifference function below.
+def minimumAbsoluteDifference(arr):
+    arr.sort()
+    return min(arr[i+1] - arr[i] for i in range(len(arr)-1))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = minimumAbsoluteDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
